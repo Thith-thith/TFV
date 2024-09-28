d0518b8e6d7fe5e34367d1cad6b4865c04e2ff28
Thith-thith <thiththin762@gmail.com>
2024-09-28 11:57:17 -0400

    updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'https://www.reely.site'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const routes = ['', '/movies', '/tv-shows'].map((route) => ({
