ed41cc29a24f1c163c2ff838d3e8335f5857fe33
Thith-thith <thiththin762@gmail.com>
2024-09-28 03:53:40 -0400

    updated

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'image.tmdb.org', 'plus.unsplash.com'],
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_TMDB_BASEURL:
+      process.env.NEXT_PUBLIC_TMDB_BASEURL || 'https://api.themoviedb.org/3',
+  },
 }
 
 module.exports = nextConfig
